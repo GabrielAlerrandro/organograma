29d798ad6efa2f9a318f0cd8f7aaed3356a90fa4
Gabriel Alerrandro <gabrielalerrandro081@gmail.com>
2024-06-04 17:19:05 -0300

    criação do formulario

---
+++ src/components/Formulario/Formulario.js
@@ -0,0 +1,66 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto/CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+import { useState } from 'react'
+
+const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const onSave = (evento) =>{
+        evento.preventDefault()
+    }
+    return (
+        <section className="formulario">
+            <form onSubmit = {onSave}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto 
+                obrigatorio = {true} 
+                label="Nome" 
+                placeholder="Digite seu nome" 
+                valor = {nome}
+                aoAlterado = {valor => setNome(valor)}
+                />
+                <CampoTexto 
+                obrigatorio = {true} 
+                label="Cargo" 
+                placeholder="Digite seu cargo" 
+                valor = {cargo}
+                aoAlterado = {valor => setCargo(valor)}
+                />
+                <CampoTexto 
+                obrigatorio = {true} 
+                label="Imagem" 
+                placeholder="Digite o endereço da imagem" 
+                valor = {imagem}
+                aoAlterado = {valor => setImagem(valor)}
+                />
+                <ListaSuspensa 
+                obrigatorio = {true} 
+                itens={times} 
+                label="Time"
+                valor = {time}
+                aoAlterado = {valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
