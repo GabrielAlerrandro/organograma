93a187785ba0f1d17dfa36888a58a620b5ab0e9d
Gabriel Alerrandro <gabrielalerrandro081@gmail.com>
2024-06-20 21:55:20 -0300

    ocultando times vazios

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -4,18 +4,20 @@ import "./Time.css";
 const Time = ({ nome, corPrimaria, corSecundaria, colaboradores }) => {
   const estilo = { backgroundColor: corSecundaria };
   return (
-    <section className="time" style={estilo}>
-      <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
-      <div className="colaboradores">
-        {colaboradores.map((colaborador) => (
-          <Colaborador
-            nome={colaborador.nome}
-            cargo={colaborador.cargo}
-            imagem={colaborador.imagem}
-          />
-        ))}
-      </div>
-    </section>
+    colaboradores.length > 0 && (
+      <section className="time" style={estilo}>
+        <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+        <div className="colaboradores">
+          {colaboradores.map((colaborador) => (
+            <Colaborador
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 
